de2992e80eff4a6e89609b2be9de59588969480f
mitsuhori_yu <mitsuhori_y@mti.co.jp>
2017-11-21 17:50:17 +0900

    ダブルタップすると更新

--- app/src/main/java/jp/co/mitsuhori_y/mpandroidchartsample/MainActivity.kt
+++ app/src/main/java/jp/co/mitsuhori_y/mpandroidchartsample/MainActivity.kt
@@ -15,8 +15,64 @@ import com.github.mikephil.charting.listener.OnChartGestureListener
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity(), OnChartGestureListener, OnChartValueSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        show()
+    }
+
+    fun getEntries(): ArrayList<Entry> {
+        val entries: ArrayList<Entry> = ArrayList()
+        for (i in 0..27) {
+            if (i % 2 == 0) {
+                val w = Weight(Random().nextFloat() * 100, Random().nextFloat() * 20)
+                val entry = Entry(i.toFloat(), w.weight)
+                entries.add(entry)
+            }
+        }
+        return entries
+    }
+
+    fun show() {
+        val dataSet = LineDataSet(getEntries(), "Weight")
+        dataSet.lineWidth = 5.0.toFloat()
+        dataSet.color = Color.MAGENTA
+        dataSet.setCircleColor(Color.MAGENTA)
+        dataSet.setDrawCircleHole(false)
+        dataSet.circleRadius = 5f
+
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.setTouchEnabled(true)
+        chart.isDragEnabled = true
+        chart.isScaleXEnabled = true
+        chart.isScaleYEnabled = false
+        chart.setPinchZoom(true)
+        chart.isDoubleTapToZoomEnabled = false
+
+        chart.setVisibleXRangeMaximum(8.0.toFloat())
+        chart.setVisibleXRangeMinimum(1.0.toFloat())
+        chart.moveViewToX(999f)
+        // chart.centerViewTo(70.0.toFloat(), 12.0.toFloat(), YAxis.AxisDependency.LEFT)
+
+//        val xAxis = chart.xAxis
+//        xAxis.axisMaximum = 50.0.toFloat()
+//        xAxis.axisMinimum = 40.0.toFloat()
+
+        val marker: IMarker = MyMarkerView(this, R.layout.marker_view)
+        chart.marker = marker
+
+        chart.setOnChartValueSelectedListener(this)
+        chart.highlightValue(10.toFloat(), 0, true)
+        chart.onChartGestureListener = this
+
+        chart.invalidate()
+    }
+
     override fun onNothingSelected() {
         Log.i("NothingSelected", "true")
     }
@@ -51,57 +107,10 @@ class MainActivity : AppCompatActivity(), OnChartGestureListener, OnChartValueSe
 
     override fun onChartDoubleTapped(me: MotionEvent?) {
         Log.i("DoubleTapped", "true")
+        show()
     }
 
     override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {
-        Log.i("Translate", "true")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val entries: ArrayList<Entry> = ArrayList()
-        for (i in 0..1000) {
-            if (i % 2 == 0) {
-                val w = Weight(Random().nextFloat() * 100, Random().nextFloat() * 20)
-                val entry = Entry(i.toFloat(), w.weight)
-                entries.add(entry)
-            }
-        }
-
-        val dataSet = LineDataSet(entries, "Weight")
-        dataSet.lineWidth = 5.0.toFloat()
-        dataSet.color = Color.MAGENTA
-        dataSet.setCircleColor(Color.MAGENTA)
-        dataSet.setDrawCircleHole(false)
-        dataSet.circleRadius = 5f
-
-        val lineData = LineData(dataSet)
-        chart.data = lineData
-        chart.setTouchEnabled(true)
-        chart.isDragEnabled = true
-        chart.isScaleXEnabled = true
-        chart.isScaleYEnabled = false
-        chart.setPinchZoom(true)
-        chart.isDoubleTapToZoomEnabled = false
-
-        chart.setVisibleXRangeMaximum(8.0.toFloat())
-        chart.setVisibleXRangeMinimum(1.0.toFloat())
-        chart.moveViewToX(999f)
-        // chart.centerViewTo(70.0.toFloat(), 12.0.toFloat(), YAxis.AxisDependency.LEFT)
-
-//        val xAxis = chart.xAxis
-//        xAxis.axisMaximum = 50.0.toFloat()
-//        xAxis.axisMinimum = 40.0.toFloat()
-
-        val marker: IMarker = MyMarkerView(this, R.layout.marker_view)
-        chart.marker = marker
-
-        chart.setOnChartValueSelectedListener(this)
-        chart.highlightValue(10.toFloat(), 0, true)
-        chart.onChartGestureListener = this
-        
-        chart.invalidate()
+        //Log.i("Translate", "true")
     }
 }
